59e96c317d7371d0c8912b37d3bf162b75bcbf05
Abdesol <abdesoltak@gmail.com>
2021-06-30 08:07:12 +0300

    Add project files.

---
+++ YoChat/YoChat/InitializerPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace YoChat
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InitializerPage : ContentPage
+    {
+        public InitializerPage(int which_page)
+        {
+            InitializeComponent();
+            if (which_page == 0)
+            {
+                RoomCodeFrame.IsVisible = false;
+            }
+        }
+
+
+        public async void ButtonClickedGesture(object sender, EventArgs args)
+        {
+            try
+            {
+                var frame = (Frame)sender;
+                await frame.ScaleTo(0.8, 80);
+                await frame.ScaleTo(1, 80);
+                if (RoomCodeFrame.IsVisible == true)
+                {
+                    string room_code = roomcode_entry.Text;
+                    if(room_code == "ABCDE")
+                    {
+
+                        RoomCodeFrame.IsVisible = false;
+                    }
+                    else
+                    {
+                        not_exist_label.IsVisible = true;
+                    }
+                }
+                else
+                {
+                    // Navigate to the chat page
+                }
+            }
+            catch
+            {
+                var btn = (ImageButton)sender;
+                await btn.ScaleTo(0.3, 80);
+                await btn.ScaleTo(0.4, 80);
+                await Navigation.PopModalAsync(true);
+            }
+
+        }
+    }
+}
---
+++ YoChat/YoChat/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace YoChat
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void ButtonClickedGesture(object sender, EventArgs args)
+        {
+            try
+            {
+                var frame = (Frame)sender;
+                await frame.ScaleTo(0.8, 80);
+                await frame.ScaleTo(1, 80);
+
+                int which_page = 0;
+
+                if (frame == choice_btns.Children[0])
+                {
+                    Console.WriteLine("Create Room clicked");
+                }
+                else
+                {
+                    which_page = 1;
+                    Console.WriteLine("Join Room clicked");
+                }
+                await Navigation.PushModalAsync(new InitializerPage(which_page));
+            }
+            catch
+            {
+                var btn = (ImageButton)sender;
+                await btn.ScaleTo(0.3, 80);
+                await btn.ScaleTo(0.4, 80);
+            }
+
+        }
+    }
+}
